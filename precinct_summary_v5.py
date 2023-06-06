da633aca86c14094bbd30fd10f834610259bbae8
serendipitytech <troy@serendipitytech.net>
2023-06-06 13:04:33 -0400

    Update precinct_summary_v5.py

--- precinct_summary_v5.py
+++ precinct_summary_v5.py
@@ -113,27 +113,28 @@ def main():
         """)
     st.sidebar.title("Filter Selections:")
     
-    selected_elections = st.sidebar.multiselect("Select three elections:", [
-        "03-07-2023 Flagler Beach(Mar/07/2023)",
-        "03/07/2023 Flagler Beach(Mar/07/2023)",
-        "11-08-2022 General Election(Nov/08/2022)",
-        "08-23-2022 Primary Election(Aug/23/2022)",
-        "2022 City of Flagler Beach Election(Mar/08/2022)",
+    selected_elections = st.sidebar.multiselect("Select up to three elections:", [
+        "General Election(Nov/08/2022)",
+        "Primary Election(Aug/23/2022)",
+        "General Election(Nov/03/2020)",
+        "Primary Election(Aug/18/2020)",
+        "Pres Preference Primary(Mar/17/2020)",      
+ 
         "11-02-2021 Municipal Election(Nov/02/2021)",
-        "Daytona Beach Special Primary(Sep/21/2021)",
         "Municipal Election(Aug/17/2021)",
         "04-13-2021 Port Orange Primary(Apr/13/2021)",
-        "City of Flagler Beach(Mar/02/2021)",
-        "20201103 General Election(Nov/03/2020)",
-        "20200818 Primary Election(Aug/18/2020)",
-        "20200519 Pierson Mail Ballot Elec(May/19/2020)",
-        "20200317 Pres Preference Primary(Mar/17/2020)",
-        "City of Flagler Beach(Mar/17/2020)",
-        "20191105 Lake Helen General(Nov/05/2019)",
         "20190611 Pt Orange Special Runoff(Jun/11/2019)",
+        "20190430 Pt Orange Special Primary(Apr/30/2019)",     
+        "Daytona Beach Special Primary(Sep/21/2021)",
+        "20200519 Pierson Mail Ballot Elec(May/19/2020)",
+        "20191105 Lake Helen General(Nov/05/2019)",
         "20190521 Mail Ballot Election(May/21/2019)",
-        "20190430 Pt Orange Special Primary(Apr/30/2019)",
-        "20190402 Edgewater Special General(Apr/02/2019)"
+        "20190402 Edgewater Special General(Apr/02/2019)",
+        "03-07-2023 Flagler Beach(Mar/07/2023)",
+        "03/07/2023 Flagler Beach(Mar/07/2023)",
+        "2022 City of Flagler Beach Election(Mar/08/2022)",
+        "City of Flagler Beach(Mar/02/2021)",
+        "City of Flagler Beach(Mar/17/2020)"
     ], key="elections")
 
     
