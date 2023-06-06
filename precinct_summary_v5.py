8e1d08852ac0abe410b8cc3e02be4c9b13bcf7f7
serendipitytech <troy@serendipitytech.net>
2023-06-06 19:20:30 -0400

    Update precinct_summary_v5.py

--- precinct_summary_v5.py
+++ precinct_summary_v5.py
@@ -216,7 +216,7 @@ def main():
         autosize=True,
         width=800,  # Adjust width
         height=600,  # Adjust height
-        margin=dict(l=20, r=20, t=20, b=20),,
+        margin=dict(l=20, r=20, t=20, b=20),
     )
     st.plotly_chart(fig2)
 
