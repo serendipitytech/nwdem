33b2acce6ff4e99576388a68abe5bf03a8eaf1c9
serendipitytech <troy@serendipitytech.net>
2023-06-06 12:42:02 -0400

    Update precinct_summary_v5.py

--- precinct_summary_v5.py
+++ precinct_summary_v5.py
@@ -95,8 +95,7 @@ def summarize_voting_data(df, selected_elections, selected_precincts):
     columns_for_detailed_voting_history = ["VoterID", "Race", "Sex", "Birth_Date", "Precinct"]  # replace with your actual column names
 
 
-    return summary_age, row_totals_age, column_totals_age, df, summary_voting_history, row_totals_voting_history, column_totals_voting_history, df_voting_history
-
+    return summary_age, row_totals_age, column_totals_age, df[columns_for_detailed_age], summary_voting_history, row_totals_voting_history, column_totals_voting_history, df[columns_for_detailed_voting_history]
 def load_data():
     df = pd.read_csv('https://deltonastrong-assets.s3.amazonaws.com/nw_dems_data_1.txt', delimiter=',', low_memory=False)
     return df
