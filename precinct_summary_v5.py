2e1fe8e6debbed8f37018682165e36ed324bf969
serendipitytech <troy@serendipitytech.net>
2023-06-06 14:41:41 -0400

    Update precinct_summary_v5.py

--- precinct_summary_v5.py
+++ precinct_summary_v5.py
@@ -162,7 +162,9 @@ def main():
 
    # get the summaries and detailed records
     summary_age, row_totals_age, column_totals_age, detailed_age, summary_voting_history, row_totals_voting_history, column_totals_voting_history, detailed_voting_history = summarize_voting_data(df, selected_elections, selected_precincts, selected_voter_status)
-   
+    summary_age.index = summary_age.index.to_series().replace({'M': 'Male', 'F': 'Female', 'U': 'Unreported'}, regex=True)
+    summary_voting_history.index = summary_voting_history.index.to_series().replace({'M': 'Male', 'F': 'Female', 'U': 'Unreported'}, regex=True)
+
 
    # display the summaries and download links
     #st.subheader("Voting Data Summary by Age Ranges")
