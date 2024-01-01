0d2c47d0be3dddb30c7220f07e6c97ebfd2cca5b
serendipitytech <troy@serendipitytech.net>
2024-01-01 13:32:07 -0500

    Update deltona_voters.py

--- deltona_voters.py
+++ deltona_voters.py
@@ -184,6 +184,8 @@ def page_1():
 
     st.write('<style>tr:hover {background-color: #D9BF8E;}</style>', unsafe_allow_html=True)
     st.write('<style>table {width: 50%;}</style>', unsafe_allow_html=True)
+    st.write('<style>table tr:nth-child(even) {background-color: #367d9a;}</style>', unsafe_allow_html=True)
+
 
 
 def page_2():
